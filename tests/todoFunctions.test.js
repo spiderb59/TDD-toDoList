122bdbbf3fa3313ac9f46a296500c165d91499a5
Shital Mainali <mainalishital8@gmail.com>
2024-04-25 16:29:25 +0900

    Sample for test created

--- tests/todoFunctions.test.js
+++ tests/todoFunctions.test.js
@@ -1,1 +1,11 @@
+const assert = require('chai').assert; 
+
+const { createTodo, editTodo, deleteTodo } = require('./todoFunctions');
+
+const sampleTodo = {
+    title: "Sample Todo",
+    description: "This is a sample todo item",
+    dueDate: "2024-04-30",
+    priority: "High"
+};
 
